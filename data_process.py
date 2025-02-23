7ac1ee4a16627c378441bcc5377b530ceea38ec6
YuJunZheng <yzheng9657@gmail.com>
2025-02-23 00:40:04 -0800

    script implementation

--- data_process.py
+++ data_process.py
@@ -1,47 +1,80 @@
 import pandas as pd
 import json
+import re
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+nltk.download("wordnet")
+nltk.download("omw-1.4")
+nltk.download('stopwords')
 
 """Using Movie Dataset From https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata/data"""
-MOVIE_COLUMNS = ['genres', 'keywords', 'production_companies', 'overview', 'release_date', 'status', 'title']
+MOVIE_COLUMNS = ['genres', 'keywords', 'production_companies', 'overview', 'status', 'title', 'tagline']
 JSOIN_COLUMNS = ['genres', 'keywords', 'production_companies']
 
 def get_movie_data(file_path: str) -> pd.DataFrame:
     """
-    get pandas df from a given file path with specified colum names in MOVIE_COLUMNS
+    get pandas df from a given file path with specified column names in MOVIE_COLUMNS
     :param file_path: file path
     :return: raw movies dataframe
     """
     df = pd.read_csv(file_path)
-    return df[MOVIE_COLUMNS]
+    return df[MOVIE_COLUMNS].dropna(axis=0)
 
 def data_processing(data: pd.DataFrame) -> pd.DataFrame:
     """
     Given the raw movies dataframe, filter for only released movies and turn json-format string columns into lists
     :param data: movies dataframe
-    :return: dataframe with processed columns
+    :return: dataframe with json columns processed
     """
     data = data[data.status == "Released"]
     for col in JSOIN_COLUMNS:
         data.loc[:,col] = data[col].apply(json.loads).apply(lambda row: [x['name'] for x in row])
     return data
 
+def text_preprocessing(description: str) -> str:
+    """
+    Preprocessing movie description for NLP tasks
+    1) normalization: lowercasing & remove punctuations
+    2) stop words removal
+    3) lemmatization
+    :param description: raw movie description
+    :return: description after text pre-processing
+    """
+    lemmatizer = WordNetLemmatizer()
+    cleaned_tokens = []
+    tokens = word_tokenize(description)
+    # removes any character that is not a letter, number, or space
+    pattern = r'[^a-zA-Z0-9\s]'
+    stop_words = set(stopwords.words('english'))
+    for token in tokens:
+        new_token = re.sub(pattern, '', token).lower()
+        if new_token and new_token not in stop_words:
+            cleaned_tokens.append(lemmatizer.lemmatize(new_token))
+    return " ".join(cleaned_tokens)
+
 def add_movie_description(movie_attributes: pd.Series) -> str:
     """
     Generate an aggregate movie description based on all the attributes of the movie
-    :param data: movies dataframe
-    :return: df with added column "Aggregate Movie Description"
+    :param movie_attributes: genres, keywords, production companies, and overview
+    :return: aggregate movie description
     """
     movie_attributes = dict(movie_attributes)
-    genres = f"The genres are {', '.join(movie_attributes['genres'])}."
-    keywords = f"The movie has {', '.join(movie_attributes['keywords'])}."
-    production_companies = f"The movie was produced by {', '.join(movie_attributes['production_companies'])}."
-    overview = f"The movie is about {movie_attributes['overview']}."
-    release_date = f"The movie was released on {movie_attributes['release_date']}."
-    title = f"The title of the movie is {movie_attributes['title']}."
-    return " ".join([title, release_date, genres, production_companies, keywords, overview])
+
+    genres = ' '.join(movie_attributes['genres']) if movie_attributes['genres'] else ''
+    keywords = ' '.join(movie_attributes['keywords']) if movie_attributes['keywords'] else ''
+    production_companies = ' '.join(movie_attributes['production_companies']) if movie_attributes['production_companies'] else ''
+    description = " ".join([genres, keywords, movie_attributes['overview'], production_companies, movie_attributes['tagline']])
+    return text_preprocessing(description)
 
 def finalize_movie_dataset(file_name: str)-> pd.DataFrame:
-    """Prepare the movies dataset description for vectorization"""
+    """
+    Prepare the movies dataset for vectorization
+    :param file_name: movie dataset file path
+    :return: movies_df
+    """
     movies_df = get_movie_data(file_name)
     movies_df = data_processing(movies_df)
     movies_df['aggregate_description'] = movies_df.apply(add_movie_description, axis=1)
--- movie_context_recommender.py
+++ movie_context_recommender.py
@@ -1,13 +1,51 @@
 import argparse
-from data_process import finalize_movie_dataset
-import tensorflow_hub as hub
+from typing import List
 
-UNIVERSAL_ENCODER_LINK = r"https://tfhub.dev/google/universal-sentence-encoder/4"
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from data_process import finalize_movie_dataset, text_preprocessing
 
-def load_universal_sentence_encoder():
-    model = hub.load(UNIVERSAL_ENCODER_LINK)
-    print("module %s loaded" % UNIVERSAL_ENCODER_LINK)
-    return model
+RECOMMENDATION_COLUMNS = ['title', 'tagline', 'genres', 'overview', 'similarity_score']
+
+def load_tfidf(corpus: List[str]) -> np.ndarray:
+    """
+    tf-idf vectorization given a list of documents in the corpus
+    :param corpus: document list
+    :return: tf-idf vectors
+    """
+    model = TfidfVectorizer(ngram_range=(1,2), norm='l2', smooth_idf=True, max_features=500)
+    output = model.fit_transform(corpus)
+    return output.toarray()
+
+def context_recommender(user_input: str, movies_df: pd.DataFrame, encoder) -> pd.DataFrame:
+    """
+    Given a user preference string and a dataset of movies, compute the cosine similarity scores based on encoded vectors.
+    The similarity scores are ranked in descending order.
+    :param user_input: user preference string
+    :param movies_df: movies dataset
+    :param encoder: encoder function such that encoder(corpus) returns the normalized vectors for each document in the corpus
+    :return: movies df in descending order of the cosine similarity scores
+    """
+    user_input = text_preprocessing(user_input)
+    embeddings = encoder([user_input.lower()] + movies_df.aggregate_description.tolist())
+    user_embedding, movies_embeddings = embeddings[0], embeddings[1:]
+    # cosine similarity is the dot product since the vectors are already normalized
+    similarity = (movies_embeddings @ user_embedding.T).squeeze()
+    movies_df['similarity_score'] = similarity
+    return movies_df
+
+def get_top_recommendations(movie_df: pd.DataFrame, top_k:int) -> pd.DataFrame:
+    """
+    helper_function to filter for top k movies in the dataframe based on similarity score
+    :param movie_df: movies dataframe
+    :param top_k: top k integer
+    :return: top k movie recommendations
+    """
+    movie_df = movie_df.sort_values(by='similarity_score', ascending=False).reset_index(drop=True)
+    top_rec = movie_df.head(top_k)
+    return  top_rec[RECOMMENDATION_COLUMNS]
+
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -20,11 +58,19 @@ def get_args():
                         help="Enter user movie preference",
                         # TODO: remove this after testing
                         default = "I love thrilling action movies set in space, with a comedic twist.")
+    parser.add_argument("--top_k",
+                        type=int,
+                        help="enter top k recommendations wanted",
+                        # TODO: remove this after testing
+                        default=5)
     args = parser.parse_args()
     return args
 
 if __name__ == "__main__":
     args = get_args()
     data = finalize_movie_dataset(args.read_path)
-    encoder = load_universal_sentence_encoder()
-    print('hi')
+    recommendations = context_recommender(user_input=args.user_input,
+                               movies_df=data,
+                               encoder=load_tfidf)
+    top_recs = get_top_recommendations(recommendations, args.top_k)
+    print(top_recs)
